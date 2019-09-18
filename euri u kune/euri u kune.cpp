acd538e57ee875f4a46dae83bc9a4887e6f2c91d
SvenRadanovic <55486783+SvenRadanovic@users.noreply.github.com>
2019-09-18 12:37:13 +0200

    Add files via upload

---
+++ euri u kune/euri u kune.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main() 
+{
+	float kune,a,b;
+	kune=0.135173;
+	
+	cout<<"upisi kune"<<endl;
+	cin>>a;
+	b=a*kune;
+	cout<<a<<" kuna je "<<b<<"eura "<<endl;
+	return 0;
+}
